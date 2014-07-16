07952a1b4710db19d0433ad773b8cd2d4ce6f55f
mix3 <himachocost333@hotmail.co.jp>
2014-07-16 12:53:01 +0900

    pingすればidleにならない？

--- webapp/controller/controller.go
+++ webapp/controller/controller.go
@@ -10,3 +10,8 @@ func TopPage(c rocket.CtxData) {
 	c.Res().StatusCode = http.StatusOK
 	c.RenderText("Hello World")
 }
+
+func PingPage(c rocket.CtxData) {
+	c.Res().StatusCode = http.StatusOK
+	c.RenderText("OK")
+}
--- webapp/webapp.go
+++ webapp/webapp.go
@@ -2,6 +2,9 @@ package webapp
 
 import (
 	"net"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/acidlemon/rocket"
 	"github.com/mix3/go-rocket-sample-app/webapp/controller"
@@ -25,12 +28,29 @@ func NewWebApp() WebApp {
 		view.Render{},
 	)
 
+	app.AddRoute(
+		"/ping",
+		controller.PingPage,
+		view.Render{},
+	)
+
 	app.BuildRouter()
 
 	return app
 }
 
 func Start(listener net.Listener) {
+	go ping()
 	app := NewWebApp()
 	app.Start(listener)
+}
+
+func ping() {
+	ticker := time.NewTicker(time.Minute * 50)
+	for {
+		select {
+		case <-ticker.C:
+			http.Get(os.Getenv("PING_URL"))
+		}
+	}
 }
